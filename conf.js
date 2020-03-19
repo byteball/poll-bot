ba9f385f721a474f35f34dab09edfbd7868f40eb
tony <tonyofbyteball@users.noreply.github.com>
2020-03-19 16:54:18 +0300

    5th OP poll

--- conf.js
+++ conf.js
@@ -24,4 +24,4 @@ exports.arrEmailAttestors = ['H5EZTQE7ABFH27AUDTQFMZIALANK6RBG'];
 exports.arrRealNameAttestors = ['I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT', 'OHVQ2R5B6TUR5U7WJNYLP3FIOSR7VCED'];
 
 // whitelist of poll units that we have approved
-exports.arrPolls = ['pecgjVXpD+UaVA1Tf0WBtdleC4vtWh/EtaghX/u/vEU=', 'Jbp9eQcBHINHFVe2jHL3eHmn9ZoPWa/+qlQ1cO1vP08='];
+exports.arrPolls = ['pecgjVXpD+UaVA1Tf0WBtdleC4vtWh/EtaghX/u/vEU=', '61Ix3eHAtiGsGw6kNyRFMKyD3JSws/l8RrWzwNlJun0='];
