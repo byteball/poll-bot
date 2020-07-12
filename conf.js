f769a4e879e741e266be515846091471ac9069ed
Tarmo <tarmo888@hotmail.com>
2020-07-13 01:31:06 +0300

    new real name attestor

--- conf.js
+++ conf.js
@@ -21,7 +21,7 @@ exports.KEYS_FILENAME = 'keys.json';
 
 exports.arrSteemAttestors = ['JEDZYC2HMGDBIDQKG3XSTXUSHMCBK725'];
 exports.arrEmailAttestors = ['H5EZTQE7ABFH27AUDTQFMZIALANK6RBG'];
-exports.arrRealNameAttestors = ['I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT', 'OHVQ2R5B6TUR5U7WJNYLP3FIOSR7VCED'];
+exports.arrRealNameAttestors = ['I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT', 'JFKWGRMXP3KHUAFMF4SJZVDXFL6ACC6P', 'OHVQ2R5B6TUR5U7WJNYLP3FIOSR7VCED'];
 
 // whitelist of poll units that we have approved
 exports.arrPolls = ['pecgjVXpD+UaVA1Tf0WBtdleC4vtWh/EtaghX/u/vEU=', '0Vv6lhpjjk3VsKCSGML2NY/5W+WgpsNELQJ1rukhL5Y='];
